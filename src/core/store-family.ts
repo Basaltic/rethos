2044c43f812d75607261636fd512c4b4cf1b1e07
Lei Chen <lei91122@gmail.com>
2022-07-24 20:41:24 +0800

    fix: fix map instance does not initialze correctly

--- src/core/store-family.ts
+++ src/core/store-family.ts
@@ -1,27 +1,25 @@
-import { Store } from "./store";
-import { Identifier } from "./types";
+import { Store } from './store';
+import { Identifier } from './types';
 
 /**
  * Manage store instance with same (state, actions) structure which identified by id
  */
 export class StoreFamily {
-
-
-  private collection = new Map<Identifier, Store>;
+  private collection = new Map<Identifier, Store>();
 
   set(id: Identifier, storeInstance: Store) {
-    this.collection.set(id, storeInstance)
+    this.collection.set(id, storeInstance);
   }
 
   get(id: Identifier) {
-    return this.collection.get(id)
+    return this.collection.get(id);
   }
 
   remove(id: Identifier) {
-    this.collection.delete(id)
+    this.collection.delete(id);
   }
 
   has(id: Identifier) {
-    return this.collection.has(id)
+    return this.collection.has(id);
   }
 }
