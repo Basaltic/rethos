87d9f8a535e0542cbb8557259b111ff3af9a4c8d
Lei Chen <lei91122@gmail.com>
2022-10-04 09:21:15 +0800

    fix:  Provider type issue

--- src/react/context.tsx
+++ src/react/context.tsx
@@ -1,8 +1,8 @@
-import React, { createContext, ReactChildren } from 'react';
+import React, { createContext } from 'react';
 import { Container } from '../core/container';
 import { globalContainer } from './default';
 
 export const Context = createContext<Container>(globalContainer);
-export const Provider = (props: { value?: Container; children: ReactChildren }) => (
+export const Provider = (props: { value?: Container; children: React.ReactChild }) => (
   <Context.Provider value={props.value || globalContainer}>{props.children}</Context.Provider>
 );
